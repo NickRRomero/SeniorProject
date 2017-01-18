1f4cdf62d918812b90643adc9417812db733c0d0
Nicolas Romero <nromo01@gmail.com>
2017-01-17 19:54:41 -0800

    Added Color Spinner and Divider View

--- app/src/main/java/com/nickromero/seniorproject/views/SubscriptionFilterActivity.java
+++ app/src/main/java/com/nickromero/seniorproject/views/SubscriptionFilterActivity.java
@@ -1,23 +1,32 @@
 package com.nickromero.seniorproject.views;
 
-import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import com.nickromero.seniorproject.R;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 /**
  * Created by nickromero on 1/9/17.
  */
-public class SubscriptionFilterActivity extends AppCompatActivity{
+public class SubscriptionFilterActivity extends AppCompatActivity {
 
     private Spinner mSpinner;
 
     private Spinner mColorSpinner;
+
+    private final String[] colorNames = {"red", "pink", "purple", "deepPurple",
+            "indigo", "blue", "lightBlue", "cyan", "teal", "green", "lightGreen", "lime",
+            "yellow", "amber", "orange", "deepOrange", "brown", "grey", "blueGrey", "black"};
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -36,9 +45,49 @@ public class SubscriptionFilterActivity extends AppCompatActivity{
 
         mSpinner.setAdapter(adapter);
 
-
-
         mColorSpinner = (Spinner) findViewById(R.id.colorChooser);
 
+        mColorSpinner.setAdapter(new SpinnerAdapter(getApplicationContext(),
+                R.layout.spinner_color_chooser, colorNames));
+
+
+    }
+
+
+    /**
+     * Inner Custom Spinner Class used to populate Circluar Color Views in spinner
+     *
+     */
+    public class SpinnerAdapter extends ArrayAdapter {
+
+        public SpinnerAdapter(Context context, int resource, String[] colors) {
+            super(context, resource, colors);
+        }
+
+        @Override
+        public CircleImageView getView(int position, View convertView, ViewGroup parent) {
+
+            return getDropDownView(position, convertView, parent);
+        }
+
+        @Override
+        public CircleImageView getDropDownView(int position, View convertView, ViewGroup parent) {
+
+            LayoutInflater inflater = (LayoutInflater) getLayoutInflater();
+
+            View row = inflater.inflate(R.layout.spinner_color_chooser, parent, false);
+
+            CircleImageView circle = (CircleImageView) row.findViewById(R.id.colorCircle);
+
+
+            int resID = getResources().getIdentifier(colorNames[position], "color", getPackageName());
+
+            circle.setImageResource(resID);
+
+
+            return circle;
+
+
+        }
     }
 }
